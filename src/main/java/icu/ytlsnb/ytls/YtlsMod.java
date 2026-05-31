d81ec553cc75ef1292d9327036e4748825f3923b
ytls_66 <2371083008@qq.com>
2026-05-31 11:25:54 +0800

    代码还原

--- src/main/java/icu/ytlsnb/ytls/YtlsMod.java
+++ src/main/java/icu/ytlsnb/ytls/YtlsMod.java
@@ -1,18 +1,9 @@
 package icu.ytlsnb.ytls;
 
-import icu.ytlsnb.ytls.init.ModBusSetup;
-import icu.ytlsnb.ytls.network.ModNetwork;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-/**
- * 模组入口：仅负责把 DeferredRegister 挂到模组事件总线。
- * 具体注册条目见 {@link icu.ytlsnb.ytls.init} 包。
- */
 @Mod(ModConstants.MOD_ID)
 public final class YtlsMod {
     public YtlsMod() {
-        ModBusSetup.subscribeDeferredRegisters(FMLJavaModLoadingContext.get().getModEventBus());
-        ModNetwork.register();
     }
 }
